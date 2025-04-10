3fefd7973631d932032a1815a492d3b53df6c74d
jayzalani <151207678+jayzalani@users.noreply.github.com>
2025-04-10 23:13:06 +0530

    interview page

--- constants/index.ts
+++ constants/index.ts
@@ -104,7 +104,7 @@ export const interviewer: CreateAssistantDTO = {
   transcriber: {
     provider: "deepgram",
     model: "nova-2",
-    language: "en",
+    language: "en", 
   },
   voice: {
     provider: "11labs",
